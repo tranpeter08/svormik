0c93df4416f54e85f4a1ec69df4b6dc0cb7ff55e
Peter Tran <tran.peter08@gmail.com>
2020-02-01 15:18:27 -0800

    removed unused classes

--- classes.js
+++ classes.js
@@ -1,22 +1,9 @@
-class SvormikErrors {
-  constructor() {}
-}
-
 class SvormikStatus {
   constructor() {
     this.dirty = false;
     this.submitting = false;
+    this.hasError = false;
   }
 }
 
-class SvormikValues {
-  constructor(initialValues) {
-    if (initialValues) {
-      for (let field in initialValues) {
-        this[field] = initialValues[field];
-      }
-    }
-  }
-}
-
-module.exports = { SvormikErrors, SvormikStatus, SvormikValues };
+module.exports = {SvormikStatus};
